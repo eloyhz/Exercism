3aeb801fd824743b2451a6ae9b7c52a8c71f6f54
Eloy Hernandez <eloyhz@gmail.com>
2022-07-20 13:18:52 -0500

    [CPP] - Trinary exercise V2

--- cpp/trinary/trinary.cpp
+++ cpp/trinary/trinary.cpp
@@ -1,13 +1,13 @@
 #include "trinary.h"
+#include <regex>
 
 namespace trinary {
     int to_decimal(std::string trinary_number)   {
+        if (!std::regex_match(trinary_number, std::regex("[012]+")))    {
+            return 0;
+        }
         int decimal_value = 0;
         for (auto digit : trinary_number)    {
-            if (digit > '2' || digit < '0') {
-                decimal_value = 0;
-                break;
-            }
             decimal_value = 3 * decimal_value + (digit-'0');
         }
         return decimal_value;
